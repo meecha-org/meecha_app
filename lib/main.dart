77aa5db8979c9e1b771054135817123e34447deb
mattuu0 <tempacountyt@gmail.com>
2025-02-20 01:14:40 +0900

    meecha 動いた

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,147 @@
+// ignore_for_file: deprecated_member_use, camel_case_types
+
+import 'dart:async';
+import 'dart:convert';
+import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:flutter_inappwebview/flutter_inappwebview.dart';
+import 'package:permission_handler/permission_handler.dart';
+// import 'package:optimize_battery/optimize_battery.dart';
+import 'package:location/location.dart' as locate_dart;
+import 'package:web_socket_channel/io.dart';
+import 'package:web_socket_channel/web_socket_channel.dart';
+import 'package:web_socket_channel/status.dart' as status;
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
-void main() {
-  runApp(const MyApp());
+InAppWebViewController main_control = null as InAppWebViewController;
+String access_token = "";
+WebSocketChannel channel = null as WebSocketChannel;
+locate_dart.LocationData current_position = null as locate_dart.LocationData;
+HttpClient client = HttpClient();
+bool auto_reconnect = false;
+locate_dart.Location location = locate_dart.Location();
+bool connecting = false;
+
+final FlutterLocalNotificationsPlugin notificationsPlugin =
+    FlutterLocalNotificationsPlugin();
+const notificationDetails = NotificationDetails(
+  android: AndroidNotificationDetails(
+    '0eba0d7b-fd6a-4775-bdf7-79a82968c692',
+    'Meecha Notification',
+  ),
+);
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await LocationPermissionsHandler().request();
+  // await OptimizeBattery.stopOptimizingBatteryUsage();
+
+  Meecha_App app = const Meecha_App();
+
+  await location.enableBackgroundMode(enable: true);
+  await location.changeNotificationOptions(
+    iconName: "ic_launcher",
+    channelName: "Meecha_Core_Notify",
+    title: "Meecha",
+    subtitle: "Meecha は位置情報を使用しています",
+    onTapBringToFront: true,
+  );
+
+  location.onLocationChanged.listen((locate_dart.LocationData currentpos) {
+    debugPrint("${currentpos.latitude}, ${currentpos.longitude}");
+    //JSのイベントを呼び出す
+    try {
+      main_control.evaluateJavascript(
+        source: """
+        call_event(${currentpos.latitude}, ${currentpos.longitude});
+      """,
+      );
+    } catch (ex) {
+      debugPrint(ex.toString());
+    }
+    current_position = currentpos;
+  });
+
+  client.badCertificateCallback = (cert, host, port) => true;
+
+  try {
+    const initializationSettings = InitializationSettings(
+      android: AndroidInitializationSettings('ic_launcher'),
+    );
+
+    if (Platform.isAndroid) {
+      await notificationsPlugin
+          .resolvePlatformSpecificImplementation<
+            AndroidFlutterLocalNotificationsPlugin
+          >()
+          ?.requestNotificationsPermission();
+    }
+
+    await notificationsPlugin.initialize(
+      initializationSettings,
+
+      // 通知をタップしたときの処理(今回はprint)
+      onDidReceiveNotificationResponse: (
+        NotificationResponse notificationResponse,
+      ) async {
+        debugPrint('id=${notificationResponse.id}の通知に対してアクション。');
+      },
+    );
+  } catch (ex) {
+    debugPrint(ex.toString());
+  }
+
+  runApp(app);
 }
 
-InAppWebViewController? main_control; //as InAppWebViewController;
+enum LocationPermissionStatus { granted, denied, permanentlyDenied, restricted }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class LocationPermissionsHandler {
+  Future<bool> get isGranted async {
+    final status = await Permission.location.status;
+    switch (status) {
+      case PermissionStatus.granted:
+      case PermissionStatus.limited:
+        return true;
+      case PermissionStatus.denied:
+      case PermissionStatus.permanentlyDenied:
+      case PermissionStatus.restricted:
+        return false;
+      default:
+        return false;
+    }
+  }
+
+  Future<bool> get isAlwaysGranted {
+    return Permission.locationAlways.isGranted;
+  }
+
+  Future<LocationPermissionStatus> request() async {
+    final status = await Permission.location.request();
+    switch (status) {
+      case PermissionStatus.granted:
+        return LocationPermissionStatus.granted;
+      case PermissionStatus.denied:
+        return LocationPermissionStatus.denied;
+      case PermissionStatus.limited:
+      case PermissionStatus.permanentlyDenied:
+        return LocationPermissionStatus.permanentlyDenied;
+      case PermissionStatus.restricted:
+        return LocationPermissionStatus.restricted;
+      default:
+        return LocationPermissionStatus.denied;
+    }
+  }
+}
+
+class Meecha_App extends StatelessWidget {
+  const Meecha_App({super.key});
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'meecha_v3',
+      title: 'Meecha',
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -32,14 +159,15 @@ class MyApp extends StatelessWidget {
         // This works for code too, not just values: Most code changes can be
         // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'meecha_v3'),
+      home: const Meecha_Page(title: 'Meecha'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+class Meecha_Page extends StatefulWidget {
+  const Meecha_Page({super.key, required this.title});
 
   // This widget is the home page of your application. It is stateful, meaning
   // that it has a State object (defined below) that contains fields that affect
@@ -53,13 +181,220 @@ class MyHomePage extends StatefulWidget {
   final String title;
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<Meecha_Page> createState() => Meecha_Page_State();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class Meecha_Page_State extends State<Meecha_Page> {
+  double load_val = 0;
   bool showErrorPage = false;
   bool hideError = false;
-  double load_val = 0;
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    channel.sink.close(status.goingAway);
+  }
+
+  void init_web(List<dynamic> args) {
+    dynamic decode_data = jsonDecode(args[0] as String);
+
+    String token = decode_data["token"];
+    String wsurl = decode_data["wsurl"];
+
+    auto_reconnect = true;
+    connect(wsurl, token);
+  }
+
+  // WebSocket 切断
+  void stop_ws(List<dynamic> args) {
+    try {
+      channel.sink.close(1000);
+    } catch (ex) {
+      debugPrint(ex.toString());
+    }
+  }
+
+  void connect(String wsurl, String atoken) async {
+    connecting = true;
+    try {
+      channel.sink.close(1000);
+    } catch (ex) {
+      debugPrint(ex.toString());
+    }
+
+    Future.delayed(Duration(seconds: 5)).then((_) {
+      connecting = false;
+
+      try {
+        location
+            .changeNotificationOptions(
+              iconName: "ic_launcher",
+              channelName: "Meecha_Core_Notify",
+              title: "Meecha",
+              subtitle: "接続を開始しました",
+              onTapBringToFront: true,
+            )
+            .then((value) => null);
+      } catch (ex) {
+        debugPrint(ex.toString());
+      }
+
+      channel = IOWebSocketChannel.connect(
+        Uri.parse(wsurl),
+        customClient: client,
+        connectTimeout: Duration(seconds: 5),
+      );
+
+      channel.stream.listen(
+        (msg) {
+          bool call_js = false;
+
+          dynamic data = jsonDecode(msg);
+
+          switch (data["Command"]) {
+            case "Auth_Complete":
+              try {
+                location
+                    .changeNotificationOptions(
+                      iconName: "ic_launcher",
+                      channelName: "Meecha_Core_Notify",
+                      title: "Meecha",
+                      subtitle: "接続完了",
+                      onTapBringToFront: true,
+                    )
+                    .then((value) => null);
+
+                notificationsPlugin.cancel(10000);
+              } catch (ex) {
+                debugPrint(ex.toString());
+              }
+              break;
+            case "stop_notify":
+              try {
+                dynamic payload_data = data["Payload"];
+
+                notificationsPlugin
+                    .cancel(payload_data["userid"].hashCode)
+                    .then((value) => null);
+
+                call_js = true;
+                break;
+              } catch (ex) {
+                debugPrint(ex.toString());
+              }
+              break;
+            case "near_friend":
+              try {
+                dynamic payload_data = data["Payload"];
+
+                if (payload_data["is_first"] && payload_data["is_self"]) {
+                  try {
+                    notificationsPlugin
+                        .show(
+                          payload_data["userid"].hashCode,
+                          "Meecha",
+                          "${payload_data["unane"]}さんが近くにいます",
+                          notificationDetails,
+                        )
+                        .then((value) => null);
+                  } catch (ex) {
+                    debugPrint(ex.toString());
+                  }
+                };
+
+                call_js = true;
+                break;
+              } catch (ex) {
+                debugPrint(ex.toString());
+              }
+              break;
+            case "Location_Token":
+              access_token = data["Payload"];
+              channel.sink.add(
+                jsonEncode({
+                  "Command": "location",
+                  "Payload": {
+                    "token": access_token,
+                    "lat": current_position.latitude,
+                    "lng": current_position.longitude,
+                  },
+                }),
+              );
+              break;
+            default:
+              call_js = true;
+              break;
+          }
+
+          if (call_js) {
+            try {
+              main_control.evaluateJavascript(
+                source: """
+              on_recved(${jsonEncode(data)});
+            """,
+              );
+            } catch (ex) {
+              debugPrint(ex.toString());
+            }
+          }
+        },
+        onError: (error) {
+          debugPrint("エラーです:${error}");
+        },
+        onDone: () async {
+          debugPrint("通信を切断されました");
+          try {
+            location
+                .changeNotificationOptions(
+                  iconName: "ic_launcher",
+                  channelName: "Meecha_Core_Notify",
+                  title: "Meecha",
+                  subtitle: "切断されました",
+                  onTapBringToFront: true,
+                )
+                .then((value) => null);
+          } catch (ex) {
+            debugPrint(ex.toString());
+          }
+
+          debugPrint(channel.closeCode.toString());
+          //再接続がオフの場合戻る
+          if (!auto_reconnect) {
+            return;
+          }
+
+          //接続中の場合戻る
+          if (connecting) {
+            return;
+          }
+
+          //切断コードが1005のとき
+          if (channel.closeCode.toString() != "1000") {
+            try {
+              notificationsPlugin
+                  .show(10000, "Meecha", "切断されました", notificationDetails)
+                  .then((value) => null);
+            } catch (ex) {
+              debugPrint(ex.toString());
+            }
+            debugPrint('再接続');
+            try {
+              connect(wsurl, atoken);
+            } catch (ex) {
+              debugPrint(ex.toString());
+            }
+          }
+          ;
+        },
+      );
+
+      channel.sink.add(jsonEncode({"Command": "auth", "Payload": atoken}));
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -76,17 +411,38 @@ class _MyHomePageState extends State<MyHomePage> {
                 initialUrlRequest: URLRequest(
                   url: WebUri("https://meecha.tail6cf7b.ts.net/statics/"),
                 ),
+                androidOnGeolocationPermissionsShowPrompt: (
+                  InAppWebViewController controller,
+                  String origin,
+                ) async {
+                  return GeolocationPermissionShowPromptResponse(
+                    origin: origin,
+                    allow: true,
+                    retain: true,
+                  );
+                },
+                onReceivedServerTrustAuthRequest: (
+                  controller,
+                  challenge,
+                ) async {
+                  return ServerTrustAuthResponse(
+                    action: ServerTrustAuthResponseAction.PROCEED,
+                  );
+                },
+                initialOptions: InAppWebViewGroupOptions(
+                  android: AndroidInAppWebViewOptions(
+                    useWideViewPort: true,
+                    geolocationEnabled: true,
+                  ),
+                  ios: IOSInAppWebViewOptions(allowsInlineMediaPlayback: true),
+                ),
                 onLoadError:
                     (controller, url, code, message) => {
-                      debugPrint("Load error : ${message.toString()}"),
                       setState(() => showErrorPage = true),
                     },
                 onLoadHttpError:
                     (controller, url, statusCode, description) => {
-                      debugPrint(
-                        "Load Http error : $statusCode,URL : ${url.toString()}",
-                      ),
-                      setState(() => showErrorPage = false),
+                      setState(() => showErrorPage = true),
                     },
                 onLoadStop: (controller, url) async {
                   if (hideError) {
@@ -99,7 +455,23 @@ class _MyHomePageState extends State<MyHomePage> {
                   }
                   main_control = controller;
                 },
-                onLoadStart: (controller, url) async {},
+                onLoadStart: (controller, url) async {
+                  auto_reconnect = false;
+                  try {
+                    stop_ws([""]);
+                  } catch (ex) {
+                    debugPrint(ex.toString());
+                  }
+                  controller.addJavaScriptHandler(
+                    handlerName: 'web_inited',
+                    callback: init_web,
+                  );
+
+                  controller.addJavaScriptHandler(
+                    handlerName: 'stop_ws',
+                    callback: stop_ws,
+                  );
+                },
                 onProgressChanged: (controller, progress) {
                   try {
                     setState(() {
@@ -108,6 +480,12 @@ class _MyHomePageState extends State<MyHomePage> {
                   } catch (ex) {
                     debugPrint(ex.toString());
                   }
+                },
+                onPermissionRequest: (controller, request) async {
+                  return PermissionResponse(
+                    resources: request.resources,
+                    action: PermissionResponseAction.DENY,
+                  );
                 },
                 androidOnPermissionRequest: (
                   InAppWebViewController controller,
@@ -131,26 +509,26 @@ class _MyHomePageState extends State<MyHomePage> {
                         child: Column(
                           mainAxisAlignment: MainAxisAlignment.center,
                           children: [
-                            const Text('読み込みに失敗しました'),
+                            Text('読み込みに失敗しました'),
                             ElevatedButton(
                               onPressed: () async {
                                 hideError = true;
                                 try {
-                                  await main_control?.goBack();
+                                  await main_control.goBack();
                                 } catch (ex) {
                                   debugPrint(ex.toString());
                                 }
                               },
-                              child: const Text('戻る'),
+                              child: Text('戻る'),
                             ),
                           ],
                         ),
                       ),
                     ),
                   )
-                  : const SizedBox(height: 0, width: 0),
+                  : SizedBox(height: 0, width: 0),
               LinearProgressIndicator(
-                valueColor: const AlwaysStoppedAnimation<Color>(
+                valueColor: new AlwaysStoppedAnimation<Color>(
                   Colors.lightBlueAccent,
                 ),
                 value: load_val,
@@ -160,7 +538,7 @@ class _MyHomePageState extends State<MyHomePage> {
         ),
         onWillPop: () async {
           try {
-            await main_control?.goBack();
+            await main_control.goBack();
           } catch (ex) {
             debugPrint(ex.toString());
           }
